d79b5f3bd09da2502f49033cb7c0f1d1f81795cf
llabatut <llabatut@vulog.com>
2019-02-12 12:17:08 +0100

    fix package

--- Jenkins/client_build.sh
+++ Jenkins/client_build.sh
@@ -14,8 +14,8 @@ elif [ -f /etc/debian_version ]
 
         # Fix dh-virtualenv 1.0 install in /opt/venvs
         export DH_VIRTUALENV_INSTALL_ROOT=/opt/knock/
-        sed -i -e  's@^--extra-index-url.*$@--extra-index-url https://pypi.knock.center/root/pypi/+simple/\n--no-use-wheel@' requirements.txt
-        export NOUSEWHEEL='--no-use-wheel'
+        sed -i -e  's@^--extra-index-url.*$@--extra-index-url https://pypi.knock.center/root/pypi/+simple/@' requirements.txt
+        # export NOUSEWHEEL='--no-use-wheel'
         #Clean Server Code
         cp debian/rules_client debian/rules
 
