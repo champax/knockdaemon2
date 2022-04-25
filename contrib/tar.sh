68c8341b46c343f08c8b1f48921157d10af4a0ff
champax <champagnac.laurent@gmail.com>
2022-04-25 08:36:10 +0000

    python37 : clean

--- contrib/tar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-tar -cvzf knockdaemon2.tgz --exclude=knockdaemon2/.git --exclude=knockdaemon2/.idea --exclude=knockdaemon2/build --exclude=knockdaemon2/dist --exclude=knockdaemon2/knockdaemon2.egg-info --exclude=knockdaemon2/knockdaemon2_test --exclude=knockdaemon2/labo --exclude=knockdaemon2/salt knockdaemon2/
