a10abdd2ef525f1bb4f9601c999ef96e2a262cc8
llabatut <llabatut@vulog.com>
2025-09-10 16:01:30 +0200

    fixes 3.7 support

--- jenkins/build_client_debian.sh
+++ jenkins/build_client_debian.sh
@@ -100,6 +100,12 @@ echo "=========================="
 echo "Installing requirements.txt"
 echo "=========================="
 
+if [ `lsb_release -rs` = "10" ] ; then
+  echo "python3.7 pystemd patch"
+  sed -i 's/^pystemd$/pystemd<=0.13/' requirements.txt
+fi
+grep pystemd requirements.txt
+
 pip install -r requirements.txt --no-cache-dir
 
 echo "=========================="
